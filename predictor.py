273f0d0efe963590b0ab4e7c864b9ac635b6c5bd
tumiz <hh11698@163.com>
2026-09-03 00:06:28 +0800

    Update predictor

--- predictor.py
+++ predictor.py
@@ -1,7 +1,7 @@
 # %% Data Loading
 import pandas
 
-d = pandas.read_csv("dashboard_monthly.csv", index_col="month", usecols=["month", "aux_usd", "btc_usd", "twexb", "real_yield_pct", "fed_rate_pct"]).dropna()
+d = pandas.read_csv("dashboard_monthly.csv", index_col="month").dropna()
 
 # %% Data Preparation
 train_set = d[["aux_usd"]]
@@ -10,7 +10,9 @@ for i in range(1, 6):
     train_set[f"twexb_{i}m"] = d["twexb"].shift(i)
     train_set[f"real_yield_pct_{i}m"] = d["real_yield_pct"].shift(i)
     train_set[f"fed_rate_pct_{i}m"] = d["fed_rate_pct"].shift(i)
+    train_set[f"gpr_{i}m"] = d["gpr"].shift(i)
 train_set = train_set.dropna()
+print(train_set.index[0], len(train_set))
 
 
 # %% Model Definition
@@ -25,8 +27,11 @@ model = keras.Sequential([
 ])
 model.compile(optimizer="adam", loss="mse", metrics=["mae"])
 model.summary()
-early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=20, restore_best_weights=True)
-model.fit(train_set.drop(columns=["aux_usd"]), train_set["aux_usd"], epochs=500, batch_size=16, validation_split=0.2, callbacks=[early_stopping])
+early_stopping = keras.callbacks.EarlyStopping(min_delta=10,
+                                               monitor="val_loss", 
+                                               patience=20, 
+                                               restore_best_weights=True)
+model.fit(train_set.drop(columns=["aux_usd"]), train_set["aux_usd"], epochs=500, batch_size=4, validation_split=0.2, callbacks=[early_stopping])
 model.save("predictor_model.h5")
 plt.plot(model.history.history["loss"], label="Training Loss")
 plt.plot(model.history.history["val_loss"], label="Validation Loss")
